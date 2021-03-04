ebd909ef7762dd45fdd999bddc4424dbd3efda90
Russell Feldhausen <russfeld@ksu.edu>
2021-03-04 03:31:26 +0000

    Dist Ready

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+"""Setup file for packaging.
+
+See https://packaging.python.org/tutorials/packaging-projects/
+
+Author: Russell Feldhausen
+Version: 0.1
+"""
+
 import setuptools
 
 setuptools.setup()
---
+++ test/register/__init__.py
@@ -0,0 +1,5 @@
+"""Meta package for all cc410.register test packages.
+
+Author: Russell Feldhausen russfeld@ksu.edu
+Version: 0.1
+"""
